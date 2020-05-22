4317b4fb1cbb58818d11292012ac2f6338f71a45
Philip Lambok <philiplambok71@gmail.com>
2020-05-22 12:19:38 +0700

    Add manual comparison of encrypted field.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,4 +4,13 @@
 class Employee < ApplicationRecord
   encrypts :salary, type: :integer, encode: true
   blind_index :salary
+
+  def self.less_than(salary)
+    employees = Employee.all
+    employee_ids = []
+    employees.each do |employee|
+      employee_ids.push(employee.id) if employee.salary < salary
+    end
+    Employee.where(id: employee_ids)
+  end
 end
